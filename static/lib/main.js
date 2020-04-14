cd29451303953b3d4be6067228524d5882743a57
psychobunny <psycho.bunny@hotmail.com>
2020-04-14 16:17:30 -0400

    feat: add tooltip, closes #1 + linting

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-$(document).ready(function() {
-	var panel = $('<li class="lights-out"><a title="" href="#" data-original-title="Dark Theme"><i class="fa fa-fw fa-lightbulb-o"></i><span class="visible-xs-inline">Dark Theme</span></a></li>');
+$(document).ready(function () {
+	var panel = $('<li class="lights-out"><a title="Dark Theme" href="#"><i class="fa fa-fw fa-lightbulb-o"></i><span class="visible-xs-inline">Dark Theme</span></a></li>');
 
 	$('ul#logged-in-menu').prepend(panel);
 	$('ul#logged-out-menu').prepend(panel);
@@ -11,11 +11,16 @@ $(document).ready(function() {
 	}
 
 	var enabled = localStorage.getItem('user:theme') === 'lights-out';
-	$('body').toggleClass('lights-out',  enabled);
+	$('body').toggleClass('lights-out', enabled);
 
-	panel.on('click', function() {
+	panel.on('click', function () {
 		enabled = !$('body').hasClass('lights-out');
-		$('body').toggleClass('lights-out',  enabled);
-		localStorage.setItem('user:theme', enabled ?  'lights-out' : '');
+		$('body').toggleClass('lights-out', enabled);
+		localStorage.setItem('user:theme', enabled ? 'lights-out' : '');
+	});
+
+	panel.find('a').tooltip({
+		placement: 'bottom',
+		title: 'Dark Theme',
 	});
 });
